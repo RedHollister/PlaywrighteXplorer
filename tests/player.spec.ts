26281deb987fa921fee99fdf9a93e4db4989e358
Redhollister <strikerfire616@gmail.com>
2026-03-02 14:50:53 -0300

    Alterações no Config e criação do primeiro teste de fato

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -1,8 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test('Deve tocar uma música', async ({ page }) => {
+  const song = {
+    title: 'Bughium'
+  }
+  
+  await page.goto('/')
 
-  const loggedUser = page.locator('.logged-user');
-  await expect(loggedUser).toHaveText('Fernando Papito');
+  const loggedUser = page.locator('.logged-user')
+  await expect(loggedUser).toHaveText('Fernando Papito')
+
+  await page.click('//div[contains(@class, "song")]//h6[text()="Bughium"]/..//button')
+  await page.waitForTimeout(5000)
 });
