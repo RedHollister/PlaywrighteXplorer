51e74da65fc8f2d5714fd982fea2d8e04c1aed28
Elton Guilherme <elton.guilherme@hotmail.com>
2026-03-03 00:22:49 -0300

    Criação das variáveis play, pause e songCard

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -2,14 +2,20 @@ import { test, expect } from '@playwright/test'
 
 test('Deve tocar uma música', async ({ page }) => {
   const song = {
-    title: 'Bug Suede Shoes'
+    title: 'Bughium'
   }
-  
+
   await page.goto('/')
 
   const loggedUser = page.locator('.logged-user')
   await expect(loggedUser).toHaveText('Fernando Papito')
 
-  await page.click(`//div[contains(@class, "song")]//h6[text()="${song.title}"]/..//button`)
-  await page.waitForTimeout(5000)
+  const songCard = page.locator('.song').filter({ hasText: song.title })
+  const play = songCard.locator('.play')
+  const pause = songCard.locator('.pause')
+  await play.click()
+  await expect(pause).toBeVisible({ timeout: 5000 })
+  await expect(play).toBeVisible({ timeout: 171000 })
+
+  //await page.click(`//div[contains(@class, "song")]//h6[text()="${song.title}"]/..//button`)
 });
